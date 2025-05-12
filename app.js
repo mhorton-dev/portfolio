6b6a66fe7a370a87cd824a2c8815bcfc4f9426c2
mhorton-dev <michael@mhorton.dev>
2025-05-12 08:58:35 -0500

    Added index.html, app.js, and style.css  through home and project sections

---
+++ app.js
@@ -0,0 +1,18 @@
+const links = document.querySelectorAll('.link');
+const sections = document.querySelectorAll('section');
+
+let activeLink = 0;
+
+links.forEach((link, i) => {
+    link.addEventListener('click', () => {
+        if(activeLink != i){
+            link.classList.add('active'); 
+            sections[activeLink].classList.remove('active');
+
+            setTimeout(() => {
+                activeLink = i;
+                sections[i].classList.add('active');
+            }, 1000);
+        }
+    }, 1000);
+});
